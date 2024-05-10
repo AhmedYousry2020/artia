eac9b00ded93b833ee8ec5550bd6892d6565dbca
Mohamed Yousry <mohamed.yousry@grandmstg.com>
2024-05-10 20:04:02 +0300

    menu changes

--- app/Http/Controllers/TakkaController.php
+++ app/Http/Controllers/TakkaController.php
@@ -15,6 +15,7 @@ class TakkaController extends Controller
 
     public function index(Request $request)
     {
+        $codes=[];
         $headers = [
             'Content-Type' => 'application/json',
             'x-access-token' => 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Mzg5NywidHlwZSI6InVzZXIiLCJpYXQiOjE3MTQ0MzAzNzksImV4cCI6MTcxNzAyMjM3OX0.jjhwB_qI6OfRWKErlmGdyoQpPE3WnvIsCi3I5rDns-Q',
@@ -24,25 +25,29 @@ class TakkaController extends Controller
             "service_provider"=>"VODAFONE",
             "voucher_code"=>"VE.EV.FKA.P00300"
         ];
-        $responses = Http::pool(fn (Pool $pool) => [
-            $pool->withHeaders($headers)->post('https://dev-api.takka-pay.com/api/v2/cashcall/payments',$data),
-            $pool->withHeaders($headers)->post('https://dev-api.takka-pay.com/api/v2/cashcall/payments',$data)
-        ]);
-        dd($responses[0]->json()['data']);
-        foreach ($responses as $response) {
-            if ($response->successful()) {
-                $responseBody = $response->json();
-                $codes[] = $responseBody;
-            } else {
-                Log::error("Error uploading image", [$response->json()]);
+        try {
+            $responses = Http::pool(fn(Pool $pool) => [
+                $pool->withHeaders($headers)->post('https://dev-api.takka-pay.com/api/v2/cashcall/payments', $data),
+                $pool->withHeaders($headers)->post('https://dev-api.takka-pay.com/api/v2/cashcall/payments', $data),
+                $pool->withHeaders($headers)->post('https://dev-api.takka-pay.com/api/v2/cashcall/payments', $data)
+            ]);
+//        dd($responses[0]->json()['data']);
+            foreach ($responses as $response) {
+                if ($response->successful()) {
+                    $responseBody = $response->json();
+                    $codes[] = $responseBody['data'];
+                } else {
+                    Log::error("Error uploading image", [$response->json()]);
+                }
             }
+            return response()->json(['success' => true,"codes"=>$codes,"count"=>count($codes)]);
+
+        }catch (\Throwable $e) {
+            Log::error("takkapay", [$e->getMessage()]);
+            return response()->json(['success' => false,"codes"=>[],"count"=>0]);
+
         }
 
-//        return $responses[0]->ok() &&
-//            $responses[1]->ok() &&
-//            $responses[2]->ok();
-//
-//
-//        return response()->json(['success' => true]);
+
     }
 }
