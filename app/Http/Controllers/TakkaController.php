e09182c378ab7447f15e3f374ccdeae6044fd3c4
Mohamed Yousry <mohamed.yousry@grandmstg.com>
2024-05-18 14:45:47 +0300

    new taaka

--- app/Http/Controllers/TakkaController.php
+++ app/Http/Controllers/TakkaController.php
@@ -13,25 +13,31 @@ class TakkaController extends Controller
  {
  }
 
-    public function index(Request $request)
+    public function index(Request $request,$count)
     {
         $codes=[];
         $headers = [
             'Content-Type' => 'application/json',
-            'x-access-token' => 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Mzg5NywidHlwZSI6InVzZXIiLCJpYXQiOjE3MTQ0MzAzNzksImV4cCI6MTcxNzAyMjM3OX0.jjhwB_qI6OfRWKErlmGdyoQpPE3WnvIsCi3I5rDns-Q',
+            'x-access-token' => $request->header('x-access-token'),
         ];
+        //'x-access-token' => 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Mzg5NywidHlwZSI6InVzZXIiLCJpYXQiOjE3MTQ0MzAzNzksImV4cCI6MTcxNzAyMjM3OX0.jjhwB_qI6OfRWKErlmGdyoQpPE3WnvIsCi3I5rDns-Q',
+//        $data = [
+//            "payment_type"=>"VOUCHER",
+//            "service_provider"=>"VODAFONE",
+//            "voucher_code"=>"VE.EV.FKA.P00300"
+//        ];
         $data = [
             "payment_type"=>"VOUCHER",
-            "service_provider"=>"VODAFONE",
-            "voucher_code"=>"VE.EV.FKA.P00300"
+            "service_provider"=>$request->service_provider,
+            "voucher_code"=>$request->voucher_code
         ];
+
+        //$request_hup = array_fill(0, $count, $pool->withHeaders($headers)->post('https://dev-api.takka-pay.com/api/v2/cashcall/payments', $data));
         try {
-            $responses = Http::pool(fn(Pool $pool) => [
-                $pool->withHeaders($headers)->post('https://dev-api.takka-pay.com/api/v2/cashcall/payments', $data),
-                $pool->withHeaders($headers)->post('https://dev-api.takka-pay.com/api/v2/cashcall/payments', $data),
-                $pool->withHeaders($headers)->post('https://dev-api.takka-pay.com/api/v2/cashcall/payments', $data)
-            ]);
-//        dd($responses[0]->json()['data']);
+            $responses = Http::pool(fn(Pool $pool) =>
+                array_fill(0, $count, $pool->withHeaders($headers)->post('https://dev-api.takka-pay.com/api/v2/cashcall/payments', $data))
+            );
+        //dd($responses);
             foreach ($responses as $response) {
                 if ($response->successful()) {
                     $responseBody = $response->json();
@@ -43,8 +49,8 @@ class TakkaController extends Controller
             return response()->json(['success' => true,"codes"=>$codes,"count"=>count($codes)]);
 
         }catch (\Throwable $e) {
-            Log::error("takkapay", [$e->getMessage()]);
-            return response()->json(['success' => false,"codes"=>[],"count"=>0]);
+            //Log::error("takkapay", [$e->getMessage()]);
+            return response()->json(['success' => false,'message'=>$e->getMessage(),"codes"=>[],"count"=>0]);
 
         }
 
